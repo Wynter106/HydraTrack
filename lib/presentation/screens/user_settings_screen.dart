29215be579362482c7792af83d6953e05163465c
wilkergonzalez <156392908+wilkergonzalez@users.noreply.github.com>
2025-11-19 22:38:58 -0700

    Creating user settings
    starting work on UI screens will most likely have default values for now.

---
+++ lib/presentation/screens/user_settings_screen.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+import '../../data/models/user_settings.dart';
+
+enum WeightUnit { lb, kg }
+enum VolumeUnit { oz, ml }
+
+// class SettingsScreen extends StatefulWidget {
